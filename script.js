dfa48542d72d96db668876c3784e6beb370e5500
Seth Martineau <martineausw@gmail.com>
2024-10-31 13:20:10 -0400

    feat: implement pseudocode in JavaScript
    Light refactoring and a bug fixes during translation.

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 /*
 FUNCTION getComputerChoice() returns string
   INIT Number variable 'choice' to random number [0, 1) multiplied by 3
-  IF choice is greater than or equal to 0 THEN
+  // fix: swap the order of conditions to prevent false positives
+  IF choice is greater than or equal to 2 THEN
     RETURN string "rock"
   ENDIF
   IF choice is greater than or equal to 1 THEN
     RETURN string "paper"
   ENDIF
-  IF choice is greater than or equal to 2 THEN
+  IF choice is greater than or equal to 0 THEN
     RETURN string "scissors"
   ENDIF
 ENDFUNCTION
 */
+
+function getComputerChoice() {     
+  let choice = Math.random() * 3;    
+                                    
+  if (choice >= 2) {                
+    return "rock";                  
+  }                                 
+
+  if (choice >= 1) {                
+    return "paper";                 
+  }                                 
+
+  if (choice >= 0) {                
+    return "scissors";              
+  }                                 
+  return "unexpected";
+}
 
 
 /*
@@ -25,8 +43,24 @@ FUNCTION getHumanChoice() returns string
     "scissors": return string 'choice'
     OTHERS: 
       display "that's not how we play rock-paper-scissors."
+      RETURN CALL getHumanChoice()
   ENDCASE
 */
+
+function getHumanChoice() {                                       
+  let choice = prompt("Rock, paper, or scissors?");               
+  choice = choice.toLowerCase();                                  
+  choice = choice.trim();                                         
+  switch (choice) {                                               
+    case "rock":                                                  
+    case "paper":                                                 
+    case "scissors":                                              
+      return choice;                                           
+    default:                                                      
+      console.log("that's not how we play rock-paper-scissors");  
+      return getHumanChoice();                                    
+  }                                                               
+}
 
 /*
 FUNCTION playRound(humanChoice, computerChoice) no return 
@@ -60,6 +94,40 @@ FUNCTION playRound(humanChoice, computerChoice) no return
 ENDFUNCTION
 */
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  let win = false;
+
+  // tie condition accounted for
+  if (humanChoice === computerChoice) {
+    return;
+  }
+
+  // check for win, otherwise loss
+  // simplified this block to a boolean expression for clarity
+  switch (humanChoice) {
+    case "rock":
+      win = computerChoice === "scissors";
+    case "paper":
+      win = computerChoice === "rock";
+    case "scissors":
+      win = computerChoice === "paper";
+  }
+
+  humanChoiceFormat = humanChoice.at(0).toUpperCase() + humanChoice.substring(1);
+  computerChoiceFormat = computerChoice.at(0).toUpperCase() + computerChoice.substring(1);
+
+  if (win) {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+    playerScore++;
+  } else {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+    computerScore++;
+  }
+}
+
 /*
 FUNCTION playGame() no return
   FOR SET 'rounds' to 0, 'rounds' is less than 5, increment 'rounds'
@@ -78,3 +146,21 @@ FUNCTION playGame() no return
   display "It's a tie!"
 ENDFUNCTION
 */
+
+function playGame() {
+  for (let rounds = 0; rounds < 5; rounds++) {
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+
+  if (humanScore > computerScore) {
+    console.log("You win the game!");
+    return;
+  } 
+
+  if (humanScore < computerScore) {
+    console.log("You lose the game!");
+    return;
+  }
+
+  console.log("It's a tie!"); 
+}
