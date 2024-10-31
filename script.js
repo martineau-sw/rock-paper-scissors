cc2b285393e66015b1f7fd86bf6ef82bba35b7ff
Seth Martineau <martineausw@gmail.com>
2024-10-31 09:53:46 -0400

    chore: add pseudocode in comments of script.js

---
+++ script.js
@@ -0,0 +1,61 @@
+/*
+
+FUNCTION getComputerChoice() returns string
+  INIT Number variable 'choice' to random number [0, 1) multiplied by 3
+  IF choice is greater than or equal to 0 THEN
+    RETURN string "rock"
+  ENDIF
+  IF choice is greater than or equal to 1 THEN
+    RETURN string "paper"
+  ENDIF
+  IF choice is greater than or equal to 2 THEN
+    RETURN string "scissors"
+  ENDIF
+
+*/
+
+
+/*
+FUNCTION getHumanChoice() returns string
+  INIT string variable 'choice' to CALL prompt "Rock, paper, or scissors?"
+  SET 'choice' to 'choice' lowercased
+  SET 'choice' to 'choice' trimmed
+  CASE 'choice' OF
+    "rock": fall-through
+    "paper": fall-through
+    "scissors": return string 'choice'
+    OTHERS: 
+      display "that's not how we play rock-paper-scissors."
+  ENDCASE
+*/
+
+/*
+FUNCTION playRound(humanChoice, computerChoice) no return 
+  INIT 'win' to false
+  IF 'humanChoice' is equal to 'computerChoice' 
+    return undefined
+  CASE 'human' OF
+    "rock":
+      IF 'computerChoice' is equal to "scissors" THEN
+        SET 'win' to true
+      ENDIF
+      BREAK
+    "paper":
+      IF 'computerChoice' is equal to "rock" THEN
+        SET 'win' to true
+      ENDIF
+      BREAK
+    "scissors":
+      IF 'computerChoice' is equal to "scissors" THEN
+          SET 'win' to true
+        ENDIF
+        BREAK
+  ENDCASE
+  IF 'win' THEN
+    display "You win! (humanChoice) beats (computerChoice)"
+    increment 'humanScore'
+    return
+  ENDIF
+  display "You lose! (computerChoice) beats (humanChoice)"
+  increment 'computerScore'
+*/
