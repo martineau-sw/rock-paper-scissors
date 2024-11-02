b6d244ebafc64432287125eb6afb7315c0d37bde
Seth Martineau <martineausw@gmail.com>
2024-11-02 18:46:13 -0400

    feat: add elements for ui in index.html
    Remove pseudocode comments from script.js and `playGame()` call

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-/*
-FUNCTION getComputerChoice() returns string
-  INIT Number variable 'choice' to random number [0, 1) multiplied by 3
-  // fix: swap the order of conditions to prevent false positives
-  IF choice is greater than or equal to 2 THEN
-    RETURN string "rock"
-  ENDIF
-  IF choice is greater than or equal to 1 THEN
-    RETURN string "paper"
-  ENDIF
-  IF choice is greater than or equal to 0 THEN
-    RETURN string "scissors"
-  ENDIF
-ENDFUNCTION
-*/
-
 function getComputerChoice() {     
   let choice = Math.random() * 3;    
                                     
@@ -31,22 +15,6 @@ function getComputerChoice() {
   return "unexpected";
 }
 
-
-/*
-FUNCTION getHumanChoice() returns string
-  INIT string variable 'choice' to CALL prompt "Rock, paper, or scissors?"
-  SET 'choice' to 'choice' lowercased
-  SET 'choice' to 'choice' trimmed
-  CASE 'choice' OF
-    "rock": fall-through
-    "paper": fall-through
-    "scissors": return string 'choice'
-    OTHERS: 
-      display "that's not how we play rock-paper-scissors."
-      RETURN CALL getHumanChoice()
-  ENDCASE
-*/
-
 function getHumanChoice() {                                       
   let choice = prompt("Rock, paper, or scissors?");               
   choice = choice.toLowerCase();                                  
@@ -61,38 +29,6 @@ function getHumanChoice() {
       return getHumanChoice();                                    
   }                                                               
 }
-
-/*
-FUNCTION playRound(humanChoice, computerChoice) no return 
-  INIT 'win' to false
-  IF 'humanChoice' is equal to 'computerChoice' 
-    return undefined
-  CASE 'human' OF
-    "rock":
-      IF 'computerChoice' is equal to "scissors" THEN
-        SET 'win' to true
-      ENDIF
-      BREAK
-    "paper":
-      IF 'computerChoice' is equal to "rock" THEN
-        SET 'win' to true
-      ENDIF
-      BREAK
-    "scissors":
-      IF 'computerChoice' is equal to "scissors" THEN
-          SET 'win' to true
-        ENDIF
-        BREAK
-  ENDCASE
-  IF 'win' THEN
-    display "You win! (humanChoice) beats (computerChoice)"
-    increment 'humanScore'
-    return
-  ENDIF
-  display "You lose! (computerChoice) beats (humanChoice)"
-  increment 'computerScore'
-ENDFUNCTION
-*/
 
 let humanScore = 0;
 let computerScore = 0;
@@ -129,25 +65,6 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-/*
-FUNCTION playGame() no return
-  FOR SET 'rounds' to 0, 'rounds' is less than 5, increment 'rounds'
-    CALL playRound(CALL getHumanChoice, CALL getComputerChoice);
-  ENDFOR
-
-  IF humanScore is greater than computerScore THEN
-    display "You win the game!"
-    return
-  ENDIF
-
-  IF humanScore is less than computerScore THEN
-    display "You lose the game!"
-  ENDIF
-
-  display "It's a tie!"
-ENDFUNCTION
-*/
-
 function playGame() {
   for (let rounds = 0; rounds < 5; rounds++) {
     playRound(getHumanChoice(), getComputerChoice());
@@ -165,5 +82,3 @@ function playGame() {
 
   console.log("It's a tie!"); 
 }
-
-playGame();
